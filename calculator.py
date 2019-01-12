e3a6adb65cb5c77cf395d720537878134b2b94c3
ericenglin <ericenglin@gmail.com>
2019-01-12 17:23:21 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+#Write a program that defines four functions 
+#(multiply, add, subtract, and divide). 
+#These functions should not print anything, 
+#they should simply perform a mathematical 
+#operation on the two arguments and return 
+#the value.
+
+def multiply(a,b):
+	return a*b
+
+def add(a,b):
+	return a+b
+
+def subract(a,b):
+	return a-b
+
+def divide(a,b):
+	return a/b
